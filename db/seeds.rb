a60a0ce12af73f69a9db716f50227f8f28f48e96
西口広海 <>
2020-05-26 08:15:06 +0900

    add admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+# coding: utf-8
+admin = User.find_or_initialize_by(mail: Rails.application.credentials.admin_mail)
+if admin.new_record?
+  admin.update!(
+    name: "管理者", kana: "かんりしゃ", password: Rails.application.credentials.admin_password, gender: :man, blood_type: :o
+  )
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
